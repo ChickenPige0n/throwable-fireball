434c50d5a29924fc54d58673cab02280577a7645
ChickenPige0n <2336983354@qq.com>
2023-05-24 17:40:00 +0800

    Change some details

--- src/main/java/net/chickenpige0n/throwablefireball/Main.java
+++ src/main/java/net/chickenpige0n/throwablefireball/Main.java
@@ -1,11 +1,7 @@
 package net.chickenpige0n.throwablefireball;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 public class Main implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("throwablefireball");
-
 	@Override
 	public void onInitialize() {
 	}
